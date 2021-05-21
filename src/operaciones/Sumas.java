ba96c4904adccbeed65b090b1b8535aa41cc2275
@flaviordonez <flaviordonez@outlook.com>
2021-05-22 01:02:52 +0200

    commit con la operacion de division

--- src/operaciones/Sumas.java
+++ src/operaciones/Sumas.java
@@ -10,8 +10,9 @@ public class Sumas {
         double a = 2;
         double b = 3;
         double resultado = a+b;
+        double division = a /b;
         
         System.out.println("La suma de "+a +" + "+b+" = "+resultado);
-        
+        System.out.println("La division nos da: "+division);
     }
 }
